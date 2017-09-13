fcaf2aabd06012cb2b558079dbec6d8b06827cc9
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-12 18:09:48 -0700

    annoy js solution

---
+++ section9/annoy.js
@@ -0,0 +1,14 @@
+var answer = prompt("Are we there yet?");
+
+while (answer !== "yes" && answer !== "yeah") {
+  var answer = prompt("Are we there yet?");
+}
+
+alert("YAY, WE MADE IT!!!");
+
+
+// if (answer === "yes") {
+//   alert("YAY, we made it!");
+// } else {
+//   var answer = prompt("Are we there yet?");
+// }
