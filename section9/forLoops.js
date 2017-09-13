5307974273509404993825f03b7b2b2e172781f7
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-12 18:47:27 -0700

    for loops exercises solution

---
+++ section9/forLoops.js
@@ -0,0 +1,30 @@
+// 1. Print all numbers between -10 and 19
+// 2. Print all even numbers between 10 and 40
+// 3. Print all odd numbers between 300 and 333
+// 4. Print all numbers divisible by 5 and 3 between 5 and 50
+
+console.log("Print all numbers between -10 and 19");
+for (var i = -10; i < 20; i++) {
+  console.log(i);
+}
+
+console.log("Print all even numbers between 10 and 40");
+for (var i = 10; i <= 40; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+console.log("Print all odd numbers between 300 and 333");
+for (var i = 300; i <= 333; i++) {
+  if (i % 2 !== 0){
+    console.log(i);
+  }
+}
+
+console.log("Print all numbers divisible by 5 and 3 between 5 and 50");
+for (var i = 5; i <= 50; i++){
+  if (i % 3 === 0 || i % 5 === 0) {
+    console.log(i);
+  }
+}
