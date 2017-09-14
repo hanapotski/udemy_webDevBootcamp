94322d594d9fdd2575bcf768e022bed3b396be62
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-14 13:02:41 -0700

    array prob set js

--- section11/arrayprobset.js
+++ section11/arrayprobset.js
@@ -16,13 +16,12 @@ printReverse(["a", "b", "c", "d"]);
 
 function isUniform(arr){
   var first = arr[0];
-  arr.forEach(function(x){
-    if (x !== first){
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i] !== first){
       return false;
-    } else {
-      return true;
     }
-  });
+  }
+  return true;
 }
 console.log("isUniform");
 isUniform([1, 1, 1, 1]);
@@ -40,7 +39,7 @@ function sumArray(arr){
   arr.forEach(function(num){
     sum += num;
   });
-  console.log(sum);
+  return sum;
 }
 
 sumArray([1, 2, 3]); //6
@@ -65,3 +64,62 @@ console.log("max");
 max([1,2,3]);
 max([10, 3, 10, 4]);
 max([-5, 100]);
+
+
+
+// SOLUTIONS
+
+console.log("CONNECTED");
+
+function printReverse(arr){
+	for(var i = arr.length - 1; i >= 0; i--){
+		console.log(arr[i]);
+	}
+}
+
+printReverse([3,6,2,5]);
+
+//*** isUniform() ***
+
+function isUniform(arr){
+	var first = arr[0];
+	for(var i = 1; i < arr.length; i++){
+		if(arr[i] !== first){
+			return false;
+		}
+	}
+	return true;
+}
+
+// function isUniform(arr){
+// 	var first = arr[0];
+// 	arr.forEach(function(element){
+// 		if(element !== first){
+// 			return false;
+// 		}
+// 	});
+
+// 	return true;
+// }
+
+// *** sumArray() ***
+
+function sumArray(arr){
+	var total = 0;
+	arr.forEach(function(element){
+		total += element;
+	});
+	return total;
+}
+
+// *** max() ***
+
+function max(arr){
+	var max = arr[0];
+	for(var i = 1; i < arr.length; i++){
+		if(arr[i] > max){
+			max = arr[i];
+		}
+	}
+	return max;
+}
