f196e94c53983b508f5fb92759c5acabb94c11b5
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-16 15:36:30 -0700

    color game js

--- section15/colorgame.js
+++ section15/colorgame.js
@@ -10,6 +10,7 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,6 +26,7 @@ for(var i = 0; i < squares.length; i++){
       alert("Correct!");
     } else {
       this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again"
     }
   });
 }
