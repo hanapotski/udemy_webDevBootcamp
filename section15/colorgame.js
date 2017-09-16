e2947ee0298d2fc9b0078e275fcaca2a348f224b
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-16 15:34:50 -0700

    color game js

--- section15/colorgame.js
+++ section15/colorgame.js
@@ -24,7 +24,7 @@ for(var i = 0; i < squares.length; i++){
     if(clickedColor === pickedColor){
       alert("Correct!");
     } else {
-      alert("Wrong!!!");
+      this.style.backgroundColor = "#232323";
     }
   });
 }
