bdff7e2b6a9205b91bfd14caf0b9f574707cff7c
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-16 10:35:56 -0700

    score keeper js

---
+++ section13/scorekeeper.js
@@ -0,0 +1,10 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById("p2");
+var p1Display = document.querySelector("#p1Display");
+
+var p1Score = 0;
+
+p1Button.addEventListener("click", function(){
+  p1Score++;
+  p1Display.textContent = p1Score;
+});
