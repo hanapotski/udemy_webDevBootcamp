17cac60ef85ea0d08622233cdff63bef24fd11c7
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-16 14:39:20 -0700

    todo js

--- section14/todos.js
+++ section14/todos.js
@@ -1,5 +1,17 @@
-var firstLI = document.querySelector("li");
+var lis = document.querySelectorAll("li");
 
-firstLI.addEventListener("mouseover", functio(){
-  firstLI.style.color = "green";
-});
+for(var i = 0; i < lis.length; i++){
+  lis[i].addEventListener("mouseover", function(){
+    this.style.color = "green";
+  });
+}
+
+for(var i = 0; i < lis.length; i++){
+  lis[i].addEventListener("mouseout", function(){
+    this.style.color = "black";
+  });
+  lis[i].addEventListener("click", function(){
+    this.classList.toggle("done");
+  });
+}
+//this refers to the item/element triggered on
