888860f2c5b028141870159f06ca2238a97a0b56
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-15 22:32:41 -0700

    color changer js

---
+++ section14/colorChanger.js
@@ -0,0 +1,30 @@
+var btn = document.getElementById("btn");
+// var isWhite = true;
+// var color = "white";
+//
+// btn.addEventListener("click", changeColor);
+
+// function changeColor(){
+//   if (isWhite){
+//     document.body.style.backgroundColor = "purple";
+//     isWhite = false;
+//   } else {
+//     document.body.style.backgroundColor = "white";
+//     isWhite = true;
+//   }
+//  isWhite = !isWhite
+// }
+//
+// function changeColor(){
+//   if (color === "white"){
+//     document.body.style.backgroundColor = "purple";
+//     color = "purple";
+//   } else {
+//     document.body.style.backgroundColor = "white";
+//     color = "white";
+//   }
+// }
+
+btn.addEventListener("click", function(){
+  document.body.classList.toggle("purple");
+});
