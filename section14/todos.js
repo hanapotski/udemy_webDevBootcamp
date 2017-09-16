8b92beb488d62a8918febbe42548c0a7e751d902
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-16 14:43:25 -0700

    todos js

--- section14/todos.js
+++ section14/todos.js
@@ -2,14 +2,13 @@ var lis = document.querySelectorAll("li");
 
 for(var i = 0; i < lis.length; i++){
   lis[i].addEventListener("mouseover", function(){
-    this.style.color = "green";
+    this.classList.add("selected");
   });
-}
 
-for(var i = 0; i < lis.length; i++){
   lis[i].addEventListener("mouseout", function(){
-    this.style.color = "black";
+    this.classList.remove("selected");
   });
+
   lis[i].addEventListener("click", function(){
     this.classList.toggle("done");
   });
