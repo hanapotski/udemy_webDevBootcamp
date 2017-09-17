ce84919421ff3f61a116c0e403cc445c8a954a23
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-17 12:13:37 -0700

    jquery exercise js

--- section16/jqueryExercise.js
+++ section16/jqueryExercise.js
@@ -3,6 +3,7 @@ $("div").css("background", "purple");
 
 // Select the divs with class "highlight" and make them 200px wide
 $(".highlight").css("width", "200px");
+//div.highlight
 
 // Select the div with id "third" and give it a orange border
 $("#third").css("border", "2px dashed orange");
