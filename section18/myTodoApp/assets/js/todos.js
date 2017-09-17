929e62b17e274ac49eccd9736c08a6118fd04816
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-17 16:06:48 -0700

    todo js

--- section18/myTodoApp/assets/js/todos.js
+++ section18/myTodoApp/assets/js/todos.js
@@ -1,4 +1,21 @@
 // Check off Specific Todos By Clicking
 $("li").click(function(){
-  $(this).css("color", "gray");
+  //if li is gray
+  console.log($(this).css("color"));
+  if($(this).css("color") === "rgb(128, 128, 128)") {
+    //turn it black
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
+  }
+  //else
+  else {
+    //turn it gray
+    $(this).css({
+      color: "gray",
+      textDecoration: "line-through"
+    });
+  }
+
 });
