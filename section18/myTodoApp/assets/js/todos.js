b5c128a35312ac4282b7992460924272ae9a8e18
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-17 18:45:11 -0700

    todos js

--- section18/myTodoApp/assets/js/todos.js
+++ section18/myTodoApp/assets/js/todos.js
@@ -42,6 +42,6 @@ $("input[type='text']").keypress(function(event){
     // set to clear
     $(this).val("");
     // create a new li and add to ul, append(), select element to append, give a string of html
-    $("ul").append("<li><span>X </span>" + todoText + "</li>");
+    $("ul").append("<li><span><i class="fa fa-trash-o" aria-hidden="true"></i></span>" + todoText + "</li>");
   }
 });
